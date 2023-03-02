a428f912734253956e87723e0f226d71d270a33b
cosmic-blunder <sahil18bc07@gmail.com>
2023-03-03 03:01:58 +0530

    Color changing and uniform two modilfied rectangle

--- EON.Native/Native.cs
+++ EON.Native/Native.cs
@@ -3,173 +3,229 @@ using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
 using OpenTK.Windowing.GraphicsLibraryFramework;
 
-namespace EON.Native{
-  
-  
-    public class Shader {
-        int Handle;
+
+using System.Timers;
+
+namespace EON.Native
+{
+
+
+    public class Shader
+    {
+      public  int Handle;
         int VertexShader;
         int FragmentShader;
         int shader;
         int status;
-        public Shader(string vertextPath,string fragmentPath){
-            string VertexShaderSource  = File.ReadAllText(vertextPath);
+        public Shader(string vertextPath, string fragmentPath)
+        {
+            string VertexShaderSource = File.ReadAllText(vertextPath);
             string FragmentShaderSource = File.ReadAllText(fragmentPath);
 
             VertexShader = GL.CreateShader(ShaderType.VertexShader);
-            GL.ShaderSource(VertexShader,VertexShaderSource);
+            GL.ShaderSource(VertexShader, VertexShaderSource);
 
             FragmentShader = GL.CreateShader(ShaderType.FragmentShader);
-            GL.ShaderSource(FragmentShader,FragmentShaderSource);
+            GL.ShaderSource(FragmentShader, FragmentShaderSource);
 
             //Compile these shaders .
 
             GL.CompileShader(VertexShader);
-            GL.GetShader(shader,ShaderParameter.CompileStatus,out  status);
+            GL.GetShader(shader, ShaderParameter.CompileStatus, out status);
 
-            if (status == 0) {
+            if (status == 0)
+            {
                 string infoLog = GL.GetShaderInfoLog(VertexShader);
                 Console.WriteLine(infoLog);
             }
             GL.CompileShader(FragmentShader);
-            GL.GetShader(shader,ShaderParameter.CompileStatus,out  status);
+            GL.GetShader(shader, ShaderParameter.CompileStatus, out status);
 
-            if (status == 0) {
+            if (status == 0)
+            {
                 string infoLog = GL.GetShaderInfoLog(FragmentShader);
                 Console.WriteLine(infoLog);
             }
 
             Handle = GL.CreateProgram();
-            GL.AttachShader(Handle,VertexShader);
-            GL.AttachShader(Handle,FragmentShader);
+            GL.AttachShader(Handle, VertexShader);
+            GL.AttachShader(Handle, FragmentShader);
 
             GL.LinkProgram(Handle);
             int program = 0;
-            GL.GetProgram(program,GetProgramParameterName.LinkStatus,out status);
-            if (status == 0) {
+            GL.GetProgram(program, GetProgramParameterName.LinkStatus, out status);
+            if (status == 0)
+            {
                 string infoLog = GL.GetProgramInfoLog(program);
                 Console.WriteLine(infoLog);
 
             }
 
-            GL.DetachShader(Handle,VertexShader);
-            GL.DetachShader(Handle,FragmentShader);
+            GL.DetachShader(Handle, VertexShader);
+            GL.DetachShader(Handle, FragmentShader);
 
             GL.DeleteShader(FragmentShader);
             GL.DeleteShader(VertexShader);
 
         }
 
-        public void Use() {
+        public void Use()
+        {
             GL.UseProgram(Handle);
         }
 
         private bool disposedValue = false;
-        protected virtual void Dispose(bool disposing) {
-            if (disposedValue != true) {
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposedValue != true)
+            {
                 GL.DeleteProgram(Handle);
                 disposedValue = true;
             }
         }
-        ~Shader() {
+        ~Shader()
+        {
             GL.DeleteProgram(Handle);
         }
 
-       public void Disposed() {
+        public void Disposed()
+        {
             Dispose(true);
             GC.SuppressFinalize(this);
         }
     }
 
 
-public class Native:GameWindow
-{
-        string ShaderFrg = Path.GetFullPath(@"../shader/shader.frag");
-        string ShaderVert= Path.GetFullPath(@"../shader/shader.vert");
+    public class Native : GameWindow
+    {
+        static System.Timers.Timer _timer = new System.Timers.Timer(1000); //one second
 
+        string ShaderFrg = Path.GetFullPath(@"shader/shader.frag");
+        string ShaderVert = Path.GetFullPath(@"shader/shader.vert");
+
+        double elapsed;
         int VertexBufferObject;
         int ElementBufferObject;
-        public Shader? shaderP; 
+        public Shader? shaderP;
         public int VertextArrayObject;
-        public Native(int width,int height,string title):
+        public Native(int width, int height, string title) :
           base(GameWindowSettings.Default,
-                new NativeWindowSettings(){  Size=(width,height),Title=title}){  
-      }
+                new NativeWindowSettings() { Size = (width, height), Title = title })
+        {
+            _timer.AutoReset = true;
+            _timer.Enabled = true;
+            _timer.Elapsed += OnElapsed;
 
-      public float[ ] vertices = {
+        }
+        public float[] vertices = {
                 0.5f,0.5f,0.0f,//top right
                  0.5f,-0.5f,0.0f, //Bottom  right vertix
                  -0.5f,-0.5f,0.0f,//bottom left
                  -0.5f,0.5f,0.0f   //top  left
            };
 
-        uint [] indices =  {
+        uint[] indices =  {
             0,1,3,
-            1,2,3
+            0,2,3
         };
-        protected override void OnUpdateFrame(FrameEventArgs args) {
+        /**
+           
+            0       3
+
+
+            1       2    
+
+        
+        */
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
             base.OnUpdateFrame(args);
 
             KeyboardState input = KeyboardState;
 
-            if (input.IsKeyDown(Keys.Escape)) {
+            if (input.IsKeyDown(Keys.Escape))
+            {
                 Close();
             }
         }
 
-        protected override void OnLoad() {
+        protected override void OnLoad()
+        {
 
             base.OnLoad();
-            shaderP = new Shader(this.ShaderVert,this.ShaderFrg);
-            GL.ClearColor(0.2f, 0.3f,0.3f,1.0f);
-             
+            shaderP = new Shader(this.ShaderVert, this.ShaderFrg);
+            GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
             VertexBufferObject = GL.GenBuffer();
             VertextArrayObject = GL.GenVertexArray();
 
-          
+
             GL.BindVertexArray(VertextArrayObject);
 
-            GL.BindBuffer(BufferTarget.ArrayBuffer,VertexBufferObject);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
 
             GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
-            
-            ElementBufferObject = GL.GenBuffer();
-            GL.BindBuffer(BufferTarget.ElementArrayBuffer,ElementBufferObject);
-            GL.BufferData(BufferTarget.ElementArrayBuffer,indices.Length*sizeof(uint),indices,BufferUsageHint.StaticDraw);
 
-            GL.VertexAttribPointer(0,3,VertexAttribPointerType.Float,false,3*sizeof(float),0);
+            ElementBufferObject = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, ElementBufferObject);
+            GL.BufferData(BufferTarget.ElementArrayBuffer, indices.Length * sizeof(uint), indices, BufferUsageHint.StaticDraw);
+
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
             GL.EnableVertexAttribArray(0);
 
 
         }
 
-        protected override void OnRenderFrame(FrameEventArgs e) {
+
+
+        private  void OnElapsed(object? source,ElapsedEventArgs args){
+
+            this.elapsed +=1;
+
+        } 
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
             base.OnRenderFrame(e);
             GL.Clear(ClearBufferMask.ColorBufferBit);
-            
-            if(shaderP!=null){
+
+            if (shaderP != null)
+            {
 
                 shaderP?.Use();
-            }
             
-            //GL.BindVertexArray(VertextArrayObject);
-            //GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
-             GL.DrawElements(PrimitiveType.Triangles,indices.Length,DrawElementsType.UnsignedInt,0);
-            SwapBuffers();
-        }
+            //update the  uniform color
+              double timeValue = this.elapsed;
+             
+             float greenValue =   (float) Math.Sin(timeValue)/2.0f+0.5f;
 
-        protected override void OnResize(ResizeEventArgs e) {
+           
+             int vertexColorLocation = GL.GetUniformLocation(shaderP.Handle,"outColor");
+             GL.Uniform4(vertexColorLocation,0.0f,greenValue,0.0f,1.0f);
+
+             
+
+
+            GL.BindVertexArray(VertextArrayObject);
+            //GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+            GL.DrawElements(PrimitiveType.Triangles, indices.Length, DrawElementsType.UnsignedInt, 0);
+            SwapBuffers();
+            }
+        }
+        protected override void OnResize(ResizeEventArgs e)
+        {
 
             base.OnResize(e);
 
-            GL.Viewport(0,0,e.Width,e.Height);
+            GL.Viewport(0, 0, e.Width, e.Height);
         }
 
-        protected override void OnUnload() {
-            if (shaderP != null) {
-                         shaderP.Disposed();
-       
+        protected override void OnUnload()
+        {
+            if (shaderP != null)
+            {
+                shaderP.Disposed();
+
             }
-         }
+        }
     }
 }
